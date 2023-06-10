2be084130c37d21501663fa3b43d3440af581ae9
mausor11 <bartosz.danko@gmail.com>
2023-06-10 22:31:30 +0200

    cleanUPPP

--- in7.txt
+++
@@ -1,3 +0,0 @@
-ala ma kota, kot ma ale
-ala ma psa, pies nie ma ali
-kot ma psa, pies nie ma kot
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,10 @@ module HuffdecompFX {
     requires java.desktop;
 
     opens org.decompression to javafx.fxml;
+    opens org.drawtree to javafx.fxml;
 
     exports org.decompression to javafx.graphics;
     exports org.drawtree to javafx.graphics;
+    exports org.menuButton to javafx.graphics;
+    opens org.menuButton to javafx.fxml;
 }
